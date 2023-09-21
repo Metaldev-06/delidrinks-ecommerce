4d955d56d1f857707a767052e1b0a39985091b7a
Metaldev-06 <fernydiaz62@gmail.com>
2023-09-21 01:16:44 -0300

    Update: deploy

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:1337/api',
-  apiPaymentUrl: 'http://localhost:3000',
-  // imageUrl: 'http://localhost:1337',
+  apiUrl: 'https://delidrinks-ecommerce-production.up.railway.app/api',
+  apiPaymentUrl: 'https://delidrinks-ecommerce-production-682e.up.railway.app',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://delidrinks-ecommerce-production.up.railway.app/api',
-  apiPaymentUrl: 'http://localhost:3000',
+  apiPaymentUrl: 'https://delidrinks-ecommerce-production-682e.up.railway.app',
 };
